c10225bff6870597fb939181804e23a7e599de06
Run Yan <blackbird7@hotmail.com>
2022-08-11 20:47:09 +0800

    update init

--- src/watchful/__init__.py
+++ src/watchful/__init__.py
@@ -1,5 +1,18 @@
+"""
+Initializes "watchful" as a module.
+"""
+################################################################################
+
+
 import os
-
 THIS_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-
 __version__ = open(os.path.join(THIS_FILE_PATH, "VERSION"), "r").read()
+
+
+"""
+Need to be careful of unintended overriding, and also a deeper consideration
+into what should be made directly available in the "watchful" namespace.
+"""
+from .client import *
+from .attributes import *
+from .enrich import main
