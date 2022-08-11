b604c6445ce73b04f328dd37c361ba5c28b090bd
Run Yan <blackbird7@hotmail.com>
2022-08-11 20:33:01 +0800

    refactor api to client

--- tests/test_001.py
+++ tests/test_001.py
@@ -3,7 +3,7 @@
 """
 
 import watchful
-from watchful import api
+from watchful import client
 import os
 import importlib.metadata
 
@@ -11,16 +11,16 @@ THIS_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def test_version() -> None:  # pylint: disable=unused-variable  
     assert watchful.__version__ \
-            == importlib.metadata.version(__get_package_name()) \
+            == importlib.metadata.version(_get_package_name()) \
             == open(os.path.join(THIS_FILE_PATH, "..", "src", "watchful", "VERSION"), "r").read()
 
 def test_connection() -> None:  # pylint: disable=unused-variable 
-    conn = api._get_conn()
-    assert str(api.PORT).isnumeric() \
-            and conn.host == str(api.HOST) \
-            and conn.port == int(api.PORT)
+    conn = client._get_conn()
+    assert str(client.PORT).isnumeric() \
+            and conn.host == str(client.HOST) \
+            and conn.port == int(client.PORT)
 
-def __get_package_name() -> str:  # pylint: disable=unused-variable  
+def _get_package_name() -> str:  # pylint: disable=unused-variable  
     setup_data = open(os.path.join(THIS_FILE_PATH, "..", "setup.cfg"), "r")
     while line := setup_data.readline():
         if "name" in line:
