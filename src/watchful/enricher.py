13e51476295ae56e8dfc4ea4a6a9a4afeda00db8
Run Yan <blackbird7@hotmail.com>
2022-08-15 23:33:21 +0800

    source code for client data enrichment

---
+++ src/watchful/enricher.py
@@ -0,0 +1,54 @@
+"""
+This script provides the template `Enricher `class interface for adding your
+custom data enrichment functions and models. Some example enricher classes are
+provided below for your reference.
+"""
+################################################################################
+
+
+from abc import ABCMeta, abstractmethod
+from typing import Generic, Iterable, List, TypeVar
+
+try:
+    from watchful import attributes
+except (ImportError, ModuleNotFoundError):
+    import attributes
+
+
+class Enricher(metaclass=ABCMeta):
+    """
+    This is the interface that customized enricher classes should inherit, and
+    then implement the abstract methods.
+    """
+
+    @abstractmethod
+    def __init__(
+        self,
+    ) -> None:
+        """
+        In this function, we create variables that we will later use in
+        `enrich_row` to enrich our data row-wise.
+        """
+        pass
+
+    @abstractmethod
+    def enrich_row(
+        self,
+        row: Iterable[str],
+    ) -> List[attributes.TYPE_ENRICHED_CELL]:
+        """
+        In this function, we use our variables from `self.enrichment_args` to
+        enrich every row of your data. The return value is our enriched row.
+        """
+        pass
+
+    @classmethod
+    def is_enricher(
+        cls, 
+        possibly_an_enricher: Generic[TypeVar('T')]
+    ) -> bool:
+        """
+        Convenience method for checking if `possibly_an_enricher` is indeed of 
+        type `Enricher`.
+        """
+        return issubclass(possibly_an_enricher, cls)
