ea5d248d427c4fda01b8d43c98d5292f1a2ec984
Run Yan <blackbird7@hotmail.com>
2022-09-15 14:48:58 +0800

    update test for test envs

--- tests/test_001.py
+++ tests/test_001.py
@@ -3,6 +3,7 @@ Basic tests for Watchful SDK.
 """
 ################################################################################
 
+import argparse
 import os
 import sys
 import importlib.metadata
@@ -12,15 +13,21 @@ from watchful import client
 THIS_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_version() -> None:  # pylint: disable=unused-variable
-    with open(
-        os.path.join(THIS_DIR_PATH, "..", "src", "watchful", "VERSION"),
-        "r",
-        encoding=sys.getdefaultencoding()) as f:
-        version = f.readline()
-        assert watchful.__version__ \
-            == importlib.metadata.version(_get_package_name()) \
-            == version
+def test_version(test_env: str) -> None:  # pylint: disable=unused-variable
+    if test_env in ["dev", "prod", "deploy"]:
+        filename = os.path.join(THIS_DIR_PATH, "..", "VERSION_PROD_TEST") \
+            if test_env == "prod" else os.path.join(
+                THIS_DIR_PATH, "..", "src", "watchful", "VERSION"
+            )
+        with open(filename, "r", encoding=sys.getdefaultencoding()) as f:
+            version = f.readline()
+            assert watchful.__version__ \
+                == importlib.metadata.version(_get_package_name()) \
+                == version, f"versions are {watchful.__version__}, " \
+                    f"{importlib.metadata.version(_get_package_name())} and " \
+                    f"{version}."
+    else:
+        assert False, f"No such environment: {test_env}"
 
 def test_connection() -> None:  # pylint: disable=unused-variable
     conn = client._get_conn()
@@ -40,8 +47,17 @@ def _get_package_name() -> str:  # pylint: disable=unused-variable
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--test_env", type=str, default="dev",
+        help="The test environment to use, dev, prod or deploy; dev if \
+            unspecified."
+    )
+    args = parser.parse_args()
 
-    test_version()
+    test_version(args.test_env)
     test_connection()
 
     sys.exit(0)
