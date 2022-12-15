744101489b11608edb07d0aa87d6fabda44e438a
Run Yan <blackbird7@hotmail.com>
2022-12-15 19:33:12 +0800

    increase test depth

--- tests/test_0001_sdk_version_and_default_conn.py
+++ tests/test_0001_sdk_version_and_default_conn.py
@@ -4,61 +4,55 @@ This script tests Watchful SDK version.
 ################################################################################
 
 import argparse
+import importlib.metadata
 import os
 import sys
 
-# import importlib.metadata
 import watchful
 from watchful import client
 
 THIS_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_version(test_env: str) -> None:  # pylint: disable=unused-variable
+def test_version(test_env: str) -> None:
     if test_env in ["dev", "prod", "deploy"]:
         filename = (
             os.path.join(THIS_DIR_PATH, "..", "VERSION_PROD_TEST")
             if test_env == "prod"
             else os.path.join(THIS_DIR_PATH, "..", "src", "watchful", "VERSION")
         )
-        with open(filename, "r", encoding=sys.getdefaultencoding()) as f:
+        with open(filename, encoding="utf-8") as f:
             version = f.readline()
-            # assert (watchful.__version__
-            #     == importlib.metadata.version(_get_package_name())
-            #     == version
-            # ), (
-            #     f"versions are {watchful.__version__}, "
-            #     f"{importlib.metadata.version(_get_package_name())} and "
-            #     f"{version}."
-            # )
-            assert watchful.__version__ == version, (
-                f"Versions in SDK ({watchful.__version__}) and source code "
-                f"({version}) are different!"
+            import_version = importlib.metadata.version(_get_package_name())
+            assert watchful.__version__ == version == import_version, (
+                f"At least a pair of versions in SDK ({watchful.__version__}), "
+                f"source code ({version}) and import ({import_version}) is "
+                "different!"
             )
     else:
-        assert False, f"No such environment: {test_env}"
+        raise ValueError(f"No such environment: {test_env}")
 
 
-def test_connection() -> None:  # pylint: disable=unused-variable
+def test_connection() -> None:
     conn = client._get_conn()
-    assert (
-        str(client.PORT).isnumeric()
-        and conn.host == str(client.HOST)
-        and conn.port == int(client.PORT)
-    )
+    assert str(client.PORT).isnumeric(), f"{client.PORT} is not numeric!"
+    assert conn.host == str(
+        client.HOST
+    ), f"The default host ({conn.host} != {client.HOST}) is incorrect!"
+    assert conn.port == int(
+        client.PORT
+    ), f"The default port ({conn.port} != {client.PORT}) is incorrect!"
 
 
-# def _get_package_name() -> str:  # pylint: disable=unused-variable
-#     with open(
-#         os.path.join(THIS_DIR_PATH, "..", "setup.cfg"),
-#         "r",
-#         encoding=sys.getdefaultencoding()) as f:
-#         line = f.readline()
-#         while line:
-#             if "name" in line:
-#                 return line.split("=")[1].strip()
-#             line = f.readline()
-#         return ""
+def _get_package_name() -> str:
+    with open(
+        os.path.join(THIS_DIR_PATH, "..", "setup.cfg"),
+        encoding="utf-8",
+    ) as f:
+        while line := f.readline():
+            if "name" in line:
+                return line.split("=")[1].strip()
+    raise ValueError('Package "name" is not found in setup file!')
 
 
 if __name__ == "__main__":
