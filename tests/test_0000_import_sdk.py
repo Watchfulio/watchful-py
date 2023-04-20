8129fa8ca474e18d88fc2372150c08f400ee6c2e
Paul Hummer <phummer@watchful.io>
2023-04-20 13:20:24 -0600

    chore: use hatch to manage the project
    This patch uses `hatch` to manage the project, rather than trying
    to manually handle dependencies and environments, etc. Please see
    the documentation for how to use `hatch` to work on the project.
    
    This makes the minimum changes to CI to reduce disruptiveness. A
    follow up patch will sync CI with development.

--- tests/test_0000_import_sdk.py
+++ tests/test_0000_import_sdk.py
@@ -4,13 +4,10 @@ This script tests Watchful SDK imports.
 ################################################################################
 
 if __name__ == "__main__":
-
     import sys
 
-    import watchful as w
+    import watchful  # pylint:disable=W0611
     from watchful.client import *  # pylint:disable=W0401,W0614
     from watchful import *  # pylint:disable=W0401,W0614
-
-    print(f"Using Watchful SDK {w.__version__}.")
 
     sys.exit(0)
